148bbd7a37217a08da1a29c78370853c4cc7a007
Charles Moulliard <cmoulliard@apache.org>
2017-03-22 19:50:47 +0100

    Fix issue

--- src/main/java/org/jboss/OpenShiftCommand.java
+++ src/main/java/org/jboss/OpenShiftCommand.java
@@ -22,7 +22,6 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import com.openshift.restclient.ClientBuilder;
 import com.openshift.restclient.IClient;
-import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.api.model.ReplicationController;
@@ -30,17 +29,20 @@ import io.fabric8.kubernetes.api.model.ReplicationControllerBuilder;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.ServiceList;
 import io.fabric8.kubernetes.api.model.ServiceSpec;
+import io.fabric8.kubernetes.client.AutoAdaptableKubernetesClient;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class OpenShiftClient {
+public class OpenShiftCommand {
 
-    private static final Logger logger = LoggerFactory.getLogger(OpenShiftClient.class);
+    private static final Logger logger = LoggerFactory.getLogger(OpenShiftCommand.class);
 
     private CommandArgs cmdArgs;
 
@@ -49,7 +51,7 @@ public class OpenShiftClient {
         CommandArgs cmdArgs = new CommandArgs();
         JCommander cmdParser = new JCommander(cmdArgs);
         try {
-            cmdParser.setProgramName(OpenShiftClient.class.getName());
+            cmdParser.setProgramName(OpenShiftCommand.class.getName());
             cmdParser.parse(args);
         } catch (ParameterException e) {
             StringBuilder info = new StringBuilder("Specify the url of the server to access using --url\n");
@@ -59,7 +61,7 @@ public class OpenShiftClient {
         }
 
         // Let's authenticate the client
-        IClient oclient = null;
+/*        IClient oclient = null;
         if(cmdArgs.token == null) {
             oclient = new ClientBuilder(cmdArgs.url).withUserName(cmdArgs.user).withPassword(cmdArgs.password).build();
         } else {
@@ -74,22 +76,33 @@ public class OpenShiftClient {
         log("==========================");
 
         // Configure the Kubernetes client
-        Config config = new ConfigBuilder()
+/*        Config config = new ConfigBuilder()
                 .withTrustCerts(true)
                 .withUsername(cmdArgs.user)
                 .withNamespace(cmdArgs.namespace)
                 // Token is required otherwise the current user authenticated on the console (oc login) will be used
                 .withOauthToken(oclient.getAuthorizationContext().getToken())
                 .withMasterUrl(cmdArgs.url)
+                .build();*/
+        Config config = new ConfigBuilder()
+                .withTrustCerts(true)
+                .withUsername(cmdArgs.user)
+                .withPassword(cmdArgs.password)
+                //.withOauthToken(oclient.getAuthorizationContext().getToken())
+                .withNamespace(cmdArgs.namespace)
+                .withMasterUrl(cmdArgs.url)
                 .build();
 
-        try (final KubernetesClient client = new DefaultKubernetesClient(config)) {
+        KubernetesClient kubernetesClient = new DefaultKubernetesClient(config);
+        OpenShiftClient client = kubernetesClient.adapt(OpenShiftClient.class);
+
+        try {
             log("Username  : " + cmdArgs.user);
             log("Namespace : " + cmdArgs.namespace);
             log("Master URL : " + config.getMasterUrl());
             log("==========================");
 
-            log("Created RC", client.replicationControllers().inNamespace("test").create(createReplicationController()));
+            log("Created RC", client.replicationControllers().inNamespace(cmdArgs.namespace).create(createReplicationController()));
 
             // Get the RC by label
             log("Get RC by label", client.replicationControllers().withLabel("server", "nginx").list());
@@ -100,10 +113,9 @@ public class OpenShiftClient {
             listPods(client);
             listServices(client);
 
-            client.replicationControllers().inNamespace("test").withName("nginx-controller").delete();
+            client.replicationControllers().inNamespace(cmdArgs.namespace).withName("nginx-controller").delete();
             log("Deleted RC");
 
-            client.close();
         } catch (KubernetesClientException e) {
             e.printStackTrace();
             logger.error(e.getMessage(), e);
