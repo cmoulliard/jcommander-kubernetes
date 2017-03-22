5a37eaf06ba82c18ee6083ff0382c5240c9ec36c
Charles Moulliard <cmoulliard@apache.org>
2017-03-22 15:42:32 +0100

    Remove non used artifact. Format log messages

--- src/main/java/client/ListPodClient.java
+++ src/main/java/client/ListPodClient.java
@@ -15,15 +15,11 @@
  */
 package client;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.openshift.restclient.ClientBuilder;
 import com.openshift.restclient.IClient;
 import io.fabric8.kubernetes.api.model.Namespace;
@@ -37,11 +33,8 @@ import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static io.fabric8.kubernetes.client.Config.KUBERNETES_KUBECONFIG_FILE;
 
 public class ListPodClient {
 
@@ -71,61 +64,78 @@ public class ListPodClient {
             oclient = new ClientBuilder(cmdArgs.url).usingToken(cmdArgs.token).build();
         }
 
-        System.out.println("Cluster status : " + oclient.getServerReadyStatus());
-        System.out.println("Username : " + oclient.getAuthorizationContext().getUserName());
-        System.out.println("Authorized : " + oclient.getAuthorizationContext().isAuthorized());
-        System.out.println("Token : " + oclient.getAuthorizationContext().getToken());
+        log("==========================");
+        log("Cluster status : " + oclient.getServerReadyStatus());
+        log("Username : " + oclient.getAuthorizationContext().getUserName());
+        log("Authorized : " + oclient.getAuthorizationContext().isAuthorized());
+        log("Token : " + oclient.getAuthorizationContext().getToken());
+        log("==========================");
 
-        System.out.println("Pod : " + oclient.list("Pod"));
-        System.out.println("Users : " + oclient.list("User"));
+        //log("Pod : " + oclient.list("Pod"));
 
         // Configure the Kubernetes client
         Config config = new ConfigBuilder()
+                .withTrustCerts(true)
                 .withUsername(cmdArgs.user)
                 .withNamespace("default")
-                // Token is required otherwise the current user will be used
+                // Token is required otherwise the current user authentified on the console (oc login) will be used
                 .withOauthToken(oclient.getAuthorizationContext().getToken())
                 .withMasterUrl(cmdArgs.url)
                 .build();
 
         try (final KubernetesClient client = new DefaultKubernetesClient(config)) {
 
-            System.out.println("User : " + cmdArgs.user);
-            System.out.println("Namespace : " + cmdArgs.namespace);
-            System.out.println("Url : " + cmdArgs.url);
-            System.out.println("Master URL : " + config.getMasterUrl());
+            log("Username  : " + cmdArgs.user);
+            log("Namespace : " + cmdArgs.namespace);
+            log("Master URL : " + config.getMasterUrl());
+            log("==========================");
 
             Namespace namespace = client.namespaces().withName(cmdArgs.namespace).get();
-            System.out.println(
+            log(
                     "Type : " + namespace.getKind() + ", " + namespace.getMetadata().getName()
             );
 
-            PodList podList = client.pods().list();
-            List<Pod> pods = podList.getItems();
-            for(Pod pod : pods) {
-                System.out.println(
-                     "Type : " + pod.getKind() + ", " +
-                     "Name : " + pod.getMetadata().getName() + ", " +
-                     "Status : " + pod.getStatus().getPhase() + ", " +
-                     "IP : " + pod.getStatus().getPodIP()
-                );
-            }
-
-            ServiceList serviceList = client.services().list();
-            List<Service> services = serviceList.getItems();
-            for (Service service : services) {
-                ServiceSpec serviceSpec = service.getSpec();
-                System.out.println(
-                        "Name       : " + service.getMetadata().getName() + ", " +
-                        "Cluster IP : " + serviceSpec.getClusterIP() + ", " +
-                        "Port    if : " + serviceSpec.getPorts().get(0).getName()
-                );
-            }
+            listPods(client);
+            listServices(client);
 
             client.close();
         } catch (KubernetesClientException e) {
             logger.error(e.getMessage(), e);
         }
+    }
+
+    private static void listPods(KubernetesClient client) {
+        PodList podList = client.pods().list();
+        List<Pod> pods = podList.getItems();
+        log("============ Pods ===========");
+        for(Pod pod : pods) {
+            log(
+                    "Pod : " +  pod.getMetadata().getName() + ", " +
+                           "Status : " + pod.getStatus().getPhase() + ", " +
+                           "IP : " + pod.getStatus().getPodIP()
+            );
+        }
+    }
+    private static void listServices(KubernetesClient client) {
+        ServiceList serviceList = client.services().list();
+        List<Service> services = serviceList.getItems();
+        log("============ Services ===========");
+        for (Service service : services) {
+            ServiceSpec serviceSpec = service.getSpec();
+            log(
+                    "Service : " + service.getMetadata().getName() + ", " +
+                           "Cluster IP : " + serviceSpec.getClusterIP() + ", " +
+                           "Port if : " + serviceSpec.getPorts().get(0).getName()
+            );
+        }
+    }
+
+    private static void log(String action, Object obj) {
+        logger.info("{}: {}", action, obj);
+    }
+
+    private static void log(String action) {
+        logger.info(action);
     }
 
 }
